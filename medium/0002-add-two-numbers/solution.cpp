40ce033c916a5e5230199091e927cf6b717e2af1
raut-ganesh <ganeshraut2301@gmail.com>
2026-08-08 22:50:07 +0530

    ✅ Add Two Numbers | Medium | C++ | Runtime: 0 ms | Memory: 8.4 MB | 2026-08-08

---
+++ medium/0002-add-two-numbers/solution.cpp
@@ -0,0 +1,10 @@
+
+        while (l1 != nullptr || l2 != nullptr || carry != 0) {
+            int sum = carry;
+
+            if (l1 != nullptr) {
+                sum += l1->val;
+                l1 = l1->next;
+            }
+
+            if (l2 != nullptr) {
