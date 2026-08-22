9ad0e04c065faec92ce822146ca916a7bb22d8c5
Ganesh Raut <ganeshraut2301@gmail.com>
2026-08-22 07:09:59 -0700

    ✅ Sum Multiples | Easy | C++ | Runtime: 0 ms | Memory: 7.7 MB | 2026-08-22

---
+++ easy/2652-sum-multiples/solution.cpp
@@ -0,0 +1,9 @@
+                sum = sum + i;
+            }
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 || i % 5 == 0 || i % 7 == 0) {
+        }
+
+        return sum;
+    }
+};
