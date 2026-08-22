4944f03ab3814fbaf154e918eaf97a7f93788ffb
Ganesh Raut <ganeshraut2301@gmail.com>
2026-08-22 08:01:18 -0700

    ✅ Count the Digits That Divide a Number | Easy | C++ | Runtime: 0 ms (100.00%) | Memory: 7.7 MB | 2026-08-22

--- easy/2520-count-the-digits-that-divide-a-number/solution.cpp
+++ easy/2520-count-the-digits-that-divide-a-number/solution.cpp
@@ -1,16 +1,16 @@
-class Solution {
+class Solution {
 public:
-    int countDigits(int num) {
-    int count=0;
-    int n=num;
+    int countDigits(int num) {
+    int count=0;
+    int n=num;
 
-    return count;
+    while(n>0){
+        int dig = n%10;
+        n = n/10;
+        if (num%dig ==0)
+         count++;
+    }   
+    return count;
 
-    while(n>0){
-        int dig = n%10;
-    }   
-    }
-        n = n/10;
-        if (num%dig ==0)
-         count++;
+    }
 };
