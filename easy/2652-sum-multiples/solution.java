bae7b8be07869bf80c8f3389227652b55633e4e3
Ganesh Raut <ganeshraut2301@gmail.com>
2026-08-22 07:16:41 -0700

    ✅ Sum Multiples | Easy | Java | Runtime: 3 ms (93.75%) | Memory: 42.7 MB | 2026-08-22

--- easy/2652-sum-multiples/solution.java
+++ easy/2652-sum-multiples/solution.java
@@ -1,14 +1,13 @@
-class Solution {
-    public int sumOfMultiples(int n) {
-         int sum = 0;
+class Solution {
+    public int sumOfMultiples(int n) {
+         int sum = 0;
 
-        for (int i = 1; i <= n; i++) {
-            if (i % 3 == 0 || i % 5 == 0 || i % 7 == 0) {
-                sum = sum + i;
-            }
-        }
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 || i % 5 == 0 || i % 7 == 0) {
+                sum = sum + i;
+            }
+        }
 
-        return sum;
-    }
-    }
-
+        return sum;
+    }
+    }
